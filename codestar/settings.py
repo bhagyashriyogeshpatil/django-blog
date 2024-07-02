1deea3adcedcdfc5542f3aae57781db3185d196e
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-07-02 12:19:14 +0000

    Set up environment-specific SECRET_KEY handling for improved security

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$m37*mu8@@zwpjuy6m9jo$mzz&#d2$ip#pfmcf6lagdv7+z8gh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
